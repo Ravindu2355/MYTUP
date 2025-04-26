c736335611713d538c11dae08506ec9f88bdd7d3
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-04-26 15:29:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     API_ID = int(os.getenv("API_ID"))  # your telegram api_id
     API_HASH = os.getenv("API_HASH")  # your telegram api_hash
     BOT_TOKEN = os.getenv("BOT_TOKEN")
-    OWNER_ID = ins(os.getenv("OWNER"))  # your telegram user id
+    OWNER_ID = int(os.getenv("OWNER"))  # your telegram user id
